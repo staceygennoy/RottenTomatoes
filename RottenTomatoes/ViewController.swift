cfdde9f820094e9e95f75e3ee92f32ce5a84b35b
Stacey Gennoy <sgennoy@gmail.com>
2015-11-20 18:13:43 -0800

    Set dictionary JSON

--- RottenTomatoes/ViewController.swift
+++ RottenTomatoes/ViewController.swift
@@ -20,8 +20,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         movieTitles = ["The Hobbit", "Catching Fire"]
         
+        print(movieTitles[0])
+        
         tableView.delegate = self
         tableView.dataSource = self
+        
+        let url = NSURL(string: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=dagqdghwaq3e3mxyrp7kmmj5&limit=20&country=us")!
+        
+        let request = NSURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (respons: NSURLResponse?, data: NSData?, error: NSError?) -> Void in
+            
+            let dictionary = try! NSJSONSerialization.JSONObjectWithData(data!, options:[])
+            
+            print(dictionary)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,14 +42,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return movieTitles.count
         
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell") as! MovieCell
         
-        cell.titleLabel.text = "hey now"
+        let movieTitle = movieTitles[indexPath.row]
+        
+        cell.titleLabel.text = movieTitle
         cell.synopsisLabel.text = "the synopsis label"
         
         print(indexPath.row)
